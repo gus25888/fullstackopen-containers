33097b17cd4e8c69f14bca2c3b7a8012dc85ed02
Gustavo Baeza Avello <gustavobaezaavello@gmail.com>
2025-06-13 21:53:11 -0400

    Exercise 12.7 completed

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -29,18 +29,40 @@ const findByIdMiddleware = async (req, res, next) => {
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete()
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  if (req.todo) {
+    return res.json(req.todo);
+  }
+
+  return res.sendStatus(404);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const { text, done } = req.body;
+
+  if (typeof done !== "boolean") {
+    return res.status(400).json({ error: "'done' is required and must be a boolean." });
+  }
+  if (typeof text !== "string") {
+    return res.status(400).json({ error: "'text' is required and must be a string." });
+  }
+
+  const todoData = { text, done }
+
+  if (req.todo && todoData) {
+    const todoUpdated = await Todo.updateOne(req.todo, todoData, { new: true, runValidators: true, context: 'query' })
+    if (todoUpdated) {
+      return res.json(todoUpdated);
+    }
+  }
+
+  return res.sendStatus(404);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
