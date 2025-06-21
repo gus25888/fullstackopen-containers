4f255e6bb557db9f27d56651c13c826b7718e7f3
Gustavo Baeza Avello <gustavobaezaavello@gmail.com>
2025-06-20 20:48:07 -0400

    Exercise 12.13 completed

--- todo-app/todo-backend/mongo/models/Todo.js
+++ todo-app/todo-backend/mongo/models/Todo.js
@@ -5,12 +5,4 @@ const todoSchema = new mongoose.Schema({
   done: Boolean
 })
 
-todoSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  }
-});
-
 module.exports = mongoose.model('Todo', todoSchema)
